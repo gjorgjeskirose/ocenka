ac5af465ab1a69f9a7d8481f533fa4080021864f
Росе Ѓорѓески <gjorgjeskir@gmail.com>
2026-01-15 09:12:18 +0100

    1

---
+++ src/Ocenka/Ocenka.java
@@ -0,0 +1,47 @@
+package Ocenka;
+import java.util.Scanner;
+
+public class Ocenka {
+    public static void main(String[] args) {
+        double max = 0, bodovi = 0, procent = 0;
+        Scanner tastatura = new Scanner(System.in);
+        {
+            System.out.println("Vnesete go maksimalniot broj na bodovi na testot");
+            max = tastatura.nextDouble();
+        }
+
+        System.out.println("Vnesete gi bodovite koi gi osvojil studentot");
+        bodovi = tastatura.nextDouble();
+
+        procent = bodovi / max * 100;
+
+        if (bodovi > max) {
+            System.out.println("Studentot gi nadminal standardite, no za zal tolku poeni nemoze da se dobijat");
+            System.exit(0);
+        }
+
+        procent = bodovi / max * 100;
+
+        if (procent < 51) {
+            System.out.println("Studentot padnal");
+        } 
+        else if (procent <= 60 && procent >= 51) {
+            System.out.println("Studentot dobil 6");
+        } 
+        else if (procent <= 70 && procent >= 61) {
+            System.out.println("Studentot dobil 7");
+        }
+    
+    else if (procent<=80 && procent>=71) {
+    System.out.println("studentot dobil 8");
+    }
+    else if (procent<=90 && procent>=81) {
+        System.out.println("studentot dobil 9");
+    }
+    else if (procent<=100 && procent>=91) {
+        System.out.println("studentot dobil 10");
+        System.out.println("Odlicno zavrsena zadaca");
+    }
+}
+
+    }
